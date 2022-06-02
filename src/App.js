308f4d13342ec3d755564fa465b0056264d795bf
[mohadese sakhaie] <[m.sakhaie599@gmail.com]>
2022-06-02 22:24:59 +0430

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      item: [],
+      isLoaded: false,
+    };
+  }
+  componentDidMount() {
+    fetch('https://rickandmortyapi.com/api/character/1,2,3,4,5,6')
+      .then(res => res.json())
+      .then(json => {
+        this.setState({
+          isLoaded: true,
+          items: json,
+        })
+      });
+  }
+  
+  render() {
+
+    var { isLoaded, items } = this.state;
+    if (!isLoaded) {
+      return <div>loading...</div>;
+    }
+    else {
+      return (
+        <div className="App" >
+          <ul>
+            {
+              items.map(item => (
+                <li key={item.id}>
+                  id: {item.id},
+                name:{item.name}
+                  status:{item.status},
+                  species:{item.species}
+                  type:{item.type}
+                  gender: {item.gender},
+                  
+                  {/* origin:{item.origin} */}
+                </li>
+              ))
+            };
+          </ul>
+        </div>
+      );
+    }
+  }
 }
+
+
 
 export default App;
